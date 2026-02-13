330b84d15a340d74017f52c447f2dacc097a9388
Nathaniel Parah <nathanielparah579@gmail.com>
2026-02-13 12:16:54 +0100

    Validated cli_args if directory and stored it as workspace for the server
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/cl_args.rs
+++ src/cl_args.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::error::Error;
+use std::fs::metadata;
 
 pub fn get_args() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
@@ -8,7 +9,15 @@ pub fn get_args() -> Result<(), Box<dyn Error>> {
         return Err(format!("No arguments/Request were provided").into());
     }
 
-    let folder_path = &args[1];
+    let folder_path = args[1].clone();
+    
+    let dir = metadata(folder_path)?;
+
+    let my_folder = if dir.is_dir() {
+        let work_space = dir;
+    } else {
+        return Err(format!("No such file or directory").into());
+    };
 
     Ok(())
 }
