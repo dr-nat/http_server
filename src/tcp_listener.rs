fcffa56a3d47df05fc60c95ede0e476a977a13c4
Nathaniel Parah <nathanielparah579@gmail.com>
2026-02-15 16:22:11 +0100

    corrected the return type by using a generic result as return type

--- src/tcp_listener.rs
+++ src/tcp_listener.rs
@@ -1,8 +1,14 @@
 use std::net::{TcpListener, TcpStream};
 use std::error::Error;
+use std::fs::*;
+use std::io::*;
+ 
+type GenericResult<T> = std::result::Result<T, Box<dyn std::error::Error>>; 
 
-pub fn create_listener() -> Result<(), Box<dyn Error>>{
+pub fn create_listener() -> GenericResult<()>{
+
     let listener = TcpListener::bind("127.0.0.1:8080")?;
+    println!("Waiting for request: -------");
     
     for streams in listener.incoming() {
         match streams {
@@ -14,3 +20,7 @@ pub fn create_listener() -> Result<(), Box<dyn Error>>{
     Ok(())
 }
 
+//pub fn handle_request(stream: TcpStream) {
+  //  let mut stream_reader = BufReader::new(&stream);
+    
+//}
