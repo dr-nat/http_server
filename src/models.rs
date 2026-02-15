8b8fb1d5183b61277e293fb966127771494adaee
Nathaniel Parah <nathanielparah579@gmail.com>
2026-02-15 16:21:15 +0100

    Created a method that gets the method in a request
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,12 @@
 use std::collections::HashMap;
+use std::error::Error;
 
 pub enum Methods {
-    Get,
-    Patch,
-    Post,
-    Delete,
+    GET,
+    PATCH,
+    POST,
+    DELETE,
+    UNKNOWN
 }
 
 
@@ -15,3 +17,30 @@ pub struct Request {
     header: HashMap<String, String>,
     body: Option<Vec<u8>>,
 }
+
+
+impl Request {
+    
+    fn get_method(input: &str) -> Result<(), Box<dyn Error>> {
+            
+        if let Some((first_line, _)) = input.split_once("\r\n"){
+            
+            for method in first_line.split_whitespace() {
+                match method {  
+                    "Get" => Methods::GET,
+
+                    "Patch" => Methods::PATCH ,
+                    
+                    "Post" => Methods::POST,
+
+                    "Delete" => Methods::DELETE, 
+
+                    _ => Methods::UNKNOWN,
+                };
+            }
+        }
+        
+        Ok(())
+    }
+}
+
