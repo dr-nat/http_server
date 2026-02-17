e66bc8f0014cea7b067d8f8709f9b17f77916b55
Nathaniel Parah <nathanielparah579@gmail.com>
2026-02-17 11:58:06 +0100

    created a buffer and a functionality that reads bytes to it, converted it to string and passed it to my request struct to make up the constructor instance
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/tcp_listener.rs
+++ src/tcp_listener.rs
@@ -1,7 +1,8 @@
 use std::net::{TcpListener, TcpStream};
 use std::error::Error;
 use std::fs::*;
-use std::io::*;
+use std::io;
+use std::io::Read;
  
 type GenericResult<T> = std::result::Result<T, Box<dyn std::error::Error>>; 
 
@@ -20,7 +21,17 @@ pub fn create_listener() -> GenericResult<()>{
     Ok(())
 }
 
-//pub fn handle_request(stream: TcpStream) {
-  //  let mut stream_reader = BufReader::new(&stream);
+pub fn handle_request(mut stream: TcpStream) -> io::Result<()> {
+    let mut buffer = [0u8; 4096];
+
+    let num_of_bytes = stream.read(&mut buffer)?;
+
+    let bytes = &buffer[..num_of_bytes];
     
-//}
+    let converted_bytes = String::from_utf8_lossy(&bytes);
+
+    let request = Request::new(converted_bytes)?;
+
+    Ok(())
+    
+}
