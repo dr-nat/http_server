0015adc12bc5460bd55eea843e1d00431517a279
Nathaniel Parah <nathanielparah579@gmail.com>
2026-02-25 22:22:19 +0100

    Added a new module to handle incoming request
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

---
+++ src/request.rs
@@ -0,0 +1,41 @@
+use crate::models::Request;
+//use 
+use std::net::{TcpListener, TcpStream};
+use std::error::Error;
+use std::fs::*;
+use std::io::*;
+use crate::cl_args;
+ 
+// so here we create a function that reads streams to a buffer as bytes, 
+//and then convert the bytes to string then parse the string to our request struct then, 
+//call our methods on it to get the method the path and the host, 
+//and the perform specific search based on our specified path gotten from the received request.
+
+
+pub fn handle_request(mut stream: TcpStream) -> std::io::Result<()> {
+    let mut buffer = [0u8; 4096];
+
+    let incoming_streams = stream.read(&mut buffer[..])?;
+
+    let bytes = &buffer[..incoming_streams];
+
+    let converted_bytes = std::str::from_utf8(bytes).unwrap();
+
+    let request = Request::new(converted_bytes);
+
+    let extracted_request = request.unwrap();
+
+    let _method = extracted_request.get_method();
+
+    let _path = extracted_request.get_path();
+
+    let _files = cl_args::get_args();
+
+    //if method == "GET" {
+        //if path == files {
+            
+        //} 
+   // } 
+
+    Ok(())
+}
