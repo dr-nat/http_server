1ea1333b7e61c68c2c2792f8a76468cc98c28d45
Nathaniel Parah <nathanielparah579@gmail.com>
2026-03-03 22:24:48 +0100

    Changed the logic to return the path of the folder for future use in other modules
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/cl_args.rs
+++ src/cl_args.rs
@@ -1,29 +1,50 @@
 use std::env;
+use std::path::PathBuf;
 use std::error::Error;
-use std::fs::metadata;
 
-pub fn get_args() -> Result<(), Box<dyn Error>> {
+pub fn get_args() -> Result<PathBuf, Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
         return Err(format!("No arguments/Request were provided").into());
     }
 
-    let folder_path = args[1].clone();
+    let first_arg = args[1].clone();
     
     // so here we want to handle errors where if a user enters 
     // an argument that is not a directory, then we dont use it, 
     // so we convert the folder to  type of metadata, so we can 
     // use the dir method to check if path is a folder and then handle errors
     // so we store the folder in a vavriable for future  use.
-    
-    let dir = metadata(folder_path)?;
 
-    let _my_folder = if dir.is_dir() {
-        let _work_space = dir;
-    } else {
-        return Err(format!("No such file or directory").into());
-    };
+   let folder_path = PathBuf::from(first_arg);  // so here we convert the string gotten from the first argument to a path buf and then get  
+                                               // get the metadata to check if it is a directory and then return the path for future use.
 
-    Ok(())
+   folder_path.metadata()?;
+
+   if folder_path.is_dir() {
+        return Ok(folder_path);
+   } else {
+        Err("No such file or directory".into())
+   }
+
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_args() -> Result<(), Box<dyn Error>>{
+        //let path: Vec<String> = vec![r"/home/natty/Desktop".to_string()];
+        let mut path_buf = PathBuf::new();
+        path_buf.push(r"/home/natty/Desktop");
+        assert_eq!(get_args()?, path_buf);
+
+        Ok(())
+    } 
+
+
 }
