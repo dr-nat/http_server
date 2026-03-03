8e1116111969943d33eced2ef2181a7ae2369bc6
Nathaniel Parah <nathanielparah579@gmail.com>
2026-03-03 22:28:20 +0100

    created the search functionality and return of file contents
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/request.rs
+++ src/request.rs
@@ -1,5 +1,6 @@
 use crate::models::Request;
 use std::net::TcpStream;
+use std::fs::*;
 use std::io::*;
 use crate::cl_args;
  
@@ -8,7 +9,6 @@ use crate::cl_args;
 //call our methods on it to get the method the path and the host, 
 //and the perform specific search based on our specified path gotten from the received request.
 
-
 pub fn handle_request(mut stream: TcpStream) -> std::io::Result<()> {
     let mut buffer = [0u8; 4096];
 
@@ -16,23 +16,52 @@ pub fn handle_request(mut stream: TcpStream) -> std::io::Result<()> {
 
     let bytes = &buffer[..incoming_streams];
 
-    let converted_bytes = std::str::from_utf8(bytes).unwrap();
+    let converted_bytes = std::str::from_utf8(bytes).expect("Failed to convert bytes to strings");
 
     let request = Request::new(converted_bytes);
 
-    let extracted_request = request.unwrap();
+    let extracted_request = request.expect("Failed to extract the request");
 
-    let _method = extracted_request.get_method();
+    let method = extracted_request.get_method();
 
-    let _path = extracted_request.get_path();
+    let path = extracted_request.get_path();
 
-    let _files = cl_args::get_args();
+    let folder = cl_args::get_args();
 
-    //if method == "GET" {
-        //if path == files {
-            
-        //} 
-   // } 
+    let extracted_folder = folder.expect("Failed to extract the Folder"); // so we ge the folder and then read through the directory, then compare if the path gotten is found and then return the file in the html format.
+
+    if method.to_string() == "GET".to_string() {
+       let workspace = read_dir(extracted_folder)?;
+       
+       for files in workspace {
+            let file_entry = files?;
+            let file_path = &file_entry.path();
+
+            let file = file_entry.metadata();
+
+            if file?.is_file().to_string() == path {
+                let response_buffer = String::new(); 
+
+                //let file_path = file?.path();
+
+                let file_contents = std::fs::read_to_string(file_path)?; // so we get the file path and then read it to string and then format it using our function and then write it to the buffer we created. 
+
+                let formated_response = format_string_response(file_contents);
+                
+                stream.write_all(response_buffer.as_bytes())?;
+            }
+        }
+    }
 
     Ok(())
+}
+
+fn format_string_response(file_input: String) -> String {
+    
+    let status_line = "HTTP1.1/ 200 OK";
+
+    format!("{}\r\nContent-Length {}\r\nContent-Type: text/html\r\n\r\n{}", 
+        status_line, 
+        file_input.len(), 
+        file_input)
 }
