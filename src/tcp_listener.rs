5fecd5aed90940ae1b197d46c2565e8eb9b0f8e1
Nathaniel Parah <nathanielparah579@gmail.com>
2026-03-06 09:59:37 +0100

    Refactored the code to return the value within the loop
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/tcp_listener.rs
+++ src/tcp_listener.rs
@@ -1,21 +1,30 @@
-use std::net::TcpListener;
-//use std::error::Error;
-//use std::io::Read;
+use tokio::net::TcpListener;
+use crate::request::handle_request;
  
 type GenericResult<T> = std::result::Result<T, Box<dyn std::error::Error>>; 
 
-async fn create_listener() -> GenericResult<()>{
+pub async fn create_listener() -> GenericResult<()>{
 
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
     println!("Waiting for request: -------");
     
-    for streams in listener.incoming() {
-        match streams {
-            Ok(stream) => {}
-            Err(e) => {}
-        }
-    }
+    loop {
 
-    Ok(())
+        let (socket, _) = listener.accept().await?; // so the accept method returns the socket which is the stream and connection address, but we are only using the socket(connection)
+
+        tokio::spawn(async move {
+            match handle_request(socket).await {
+                Ok(_) => {},
+                Err(e) => {
+                    eprintln!("Failed to read from socket {}", e);
+                    return;
+                }
+            }
+        });
+
+        return Ok(());
+
+    }
 }
 
