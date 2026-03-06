30b428bbc740cb0130586ba3d36caaba17b53599
Nathaniel Parah <nathanielparah579@gmail.com>
2026-03-06 10:01:32 +0100

    Removed unused imports added to the main module
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use http_server::cl_args;
+use crate::tcp_listener::create_listener;
+use http_server::tcp_listener;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let request_path = cl_args::get_args();
 
     match request_path {
@@ -10,4 +13,16 @@ fn main() {
             std::process::exit(1);
         }
     }
+
+    let request_stream = create_listener();
+
+    match request_stream.await {
+        Ok(_) => {},
+        Err(e) => {
+            eprint!("Error: {}", e);
+            std::process::exit(1);
+        }
+    } 
+
+
 }
